54bee1f2f19a085f11f3da470d0999880d1bdfe4
Ajay Prajapati <prajapatisani9@gmail.com>
2023-04-29 13:44:00 +0530

    task 3

--- app.js
+++ app.js
@@ -2,21 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/addproduct',(req,res,next)=>{
-    res.send('<form action ="/product" method="POST"><input type ="text" name ="title"> <input type ="number" name ="size"><button type = "submit">Add product</button></form>')
+
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+
+app.use((req,res,next)=>{
+    res.status(404).send(`<h1>Page not found</h1>`);
 })
 
-app.post('/product', (req,res,next)=>{
-    console.log('Product route accessed');
 
-  console.log(req.body);
-  res.redirect('/');
-})
-
-app.use('/',(req,res,next)=>{
-    res.send('<h1>Hello from express</h1>');
-})
 
 app.listen(3000);
