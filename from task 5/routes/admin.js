e6ff73ef8220b813e38c81c7c808a7146da90ac6
Ajay Prajapati <prajapatisani9@gmail.com>
2023-04-30 17:40:17 +0530

    task 7 trainer

--- from task 5/routes/admin.js
+++ from task 5/routes/admin.js
@@ -15,8 +15,7 @@ router.get('/add-product', productsController.getAddProduct);
 // /admin/add-product => POST
 router.post('/add-product', productsController.postAddProduct);
 
-// exports.routes = router;
-// exports.products = products;
+
 
 
 module.exports = router;
